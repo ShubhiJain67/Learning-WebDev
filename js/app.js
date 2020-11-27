c96de83a4b4a128146b95220af2520c4fb6393d2
Shubhi Jain <shubhi.jain@altudo.co>
2020-11-27 11:46:22 +0530

    Rectified Conflict Issue

--- js/app.js
+++ js/app.js
@@ -6,31 +6,10 @@ showNotes();
 
 
 //-----------------------------------------------------------------------------------------//
-//                    If user adds a note, add it to the localStorage                      //
+//                             Adding Note to the localStorage                             //
 //-----------------------------------------------------------------------------------------//
 
 let addBtn = document.getElementById("addBtn");
-<<<<<<< Updated upstream
-addBtn.addEventListener("click", function(e) {
-  let addTxt = document.getElementById("addTxt");
-  let addTitle = document.getElementById("addTitle");
-  let notes = localStorage.getItem("notes");
-  if (notes == null) {
-    notesObj = [];
-  } else {
-    notesObj = JSON.parse(notes);
-  }
-  let myObj = {
-    title: addTitle.value,
-    text: addTxt.value
-  }
-  notesObj.push(myObj);
-  localStorage.setItem("notes", JSON.stringify(notesObj));
-  addTxt.value = "";
-  addTitle.value = "";
-//   console.log(notesObj);
-  showNotes();
-=======
 addBtn.addEventListener("click", function (e) {
     let addTxt = document.getElementById("addTxt");
     let addTitle = document.getElementById("addTitle");
@@ -50,7 +29,6 @@ addBtn.addEventListener("click", function (e) {
     addTitle.value = "";
     console.log("Added a New Note");
     showNotes();
->>>>>>> Stashed changes
 });
 
 
@@ -59,32 +37,6 @@ addBtn.addEventListener("click", function (e) {
 //-----------------------------------------------------------------------------------------//
 
 function showNotes() {
-<<<<<<< Updated upstream
-  let notes = localStorage.getItem("notes");
-  if (notes == null) {
-    notesObj = [];
-  } else {
-    notesObj = JSON.parse(notes);
-  }
-  let html = "";
-  notesObj.forEach(function(element, index) {
-    html += `
-            <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
-                    <div class="card-body">
-                        <h5 class="card-title">${element.title}</h5>
-                        <p class="card-text"> ${element.text}</p>
-                        <button id="${index}"onclick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
-                        <button id="star-${index}"onclick="starNote(this.id)" class="btn btn-primary">Star</button>
-                    </div>
-                </div>`;
-  });
-  let notesElm = document.getElementById("notes");
-  if (notesObj.length != 0) {
-    notesElm.innerHTML = html;
-  } else {
-    notesElm.innerHTML = `Nothing to show! Use "New Note" section above to add notes.`;
-  }
-=======
     let notes = localStorage.getItem("notes");
     if (notes == null) {
         notesObj = [];
@@ -111,26 +63,14 @@ function showNotes() {
     } else {
         notesElm.innerHTML = `Nothing to show! Use "New Note" section above to add notes.`;
     }
->>>>>>> Stashed changes
 }
 
 
 //-----------------------------------------------------------------------------------------//
-//                                Function to delete a note                                //
+//                                     Deleting a Note                                     //
 //-----------------------------------------------------------------------------------------//
 
 function deleteNote(index) {
-<<<<<<< Updated upstream
-  let notes = localStorage.getItem("notes");
-  if (notes == null) {
-    notesObj = [];
-  } else {
-    notesObj = JSON.parse(notes);
-  }
-  notesObj.splice(index, 1);
-  localStorage.setItem("notes", JSON.stringify(notesObj));
-  showNotes();
-=======
     if (confirm("Are you sure you want to delet it?")) {
         console.log("Deleting Card ID-", index);
         let notes = localStorage.getItem("notes");
@@ -143,7 +83,6 @@ function deleteNote(index) {
         localStorage.setItem("notes", JSON.stringify(notesObj));
         showNotes();
     }
->>>>>>> Stashed changes
 }
 
 
@@ -154,17 +93,11 @@ function deleteNote(index) {
 let search = document.getElementById('searchTxt');
 search.addEventListener("input", function () {
     let inputVal = search.value.toLowerCase();
-    console.log('Input event fired!', inputVal);
     let noteCards = document.getElementsByClassName('noteCard');
     Array.from(noteCards).forEach(function (element) {
         let cardTxt = element.getElementsByTagName("h5")[0].innerText.toLowerCase();
         let cardTxtTitle = element.getElementsByTagName("p")[0].innerText.toLowerCase();
-<<<<<<< Updated upstream
-        console.log(cardTxt);
-        if(cardTxt.includes(inputVal) || cardTxtTitle.includes(inputVal)){
-=======
         if (cardTxt.includes(inputVal) || cardTxtTitle.includes(inputVal)) {
->>>>>>> Stashed changes
             element.style.display = "block";
         }
         else {
@@ -175,22 +108,9 @@ search.addEventListener("input", function () {
 
 
 //-----------------------------------------------------------------------------------------//
-//                                      Staring Notes                                      //
+//                                     Staring a Notes                                     //
 //-----------------------------------------------------------------------------------------//
 function starNote(index) {
-<<<<<<< Updated upstream
-    let notes = localStorage.getItem("notes");
-    if (notes == null) {
-      notesObj = [];
-    } else {
-      notesObj = JSON.parse(notes);
-    }
-    localStorage.setItem("notes", JSON.stringify(notesObj));
-    let mycard = document.getElementById(index);
-    mycard.style.textDecorationColor = white;
-    showNotes();
-  }
-=======
     let mycard = document.getElementById(index);
     if (!mycard.classList.contains("bg-warning")) {
         console.log("Starred Card ID-", index);
@@ -205,7 +125,6 @@ function starNote(index) {
         mycard.getElementsByTagName("button")[1].innerText = "Star";
     }
 }
->>>>>>> Stashed changes
 
 
 /*
